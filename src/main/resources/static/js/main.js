30b9107cd90270b1c51753963f37ad615b81b740
Lastzlo <53399137+Lastzlo@users.noreply.github.com>
2020-08-21 14:55:57 +0300

    in main.js   use vue-resource for get products from server its end of Vue.js - базовый JavaScript интерфейс для REST приложения (часть 1). Spring Boot REST

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -5,7 +5,7 @@ Vue.component(
     'message-row',
     {
         props: ['message'],
-        template: '<div><i>({{ message.id }})</i> {{ message.text }}</div>',
+        template: '<div><i>({{ message.id }})</i> {{ message.name }}</div>',
     }
 )
 
@@ -13,20 +13,32 @@ Vue.component(
 // Определяем новый компонент под именем messages-list
 Vue.component('messages-list', {    //название компонента
     props: ['messages'],    //список значений которые мы ожидаем
+    //:message="message" предает параметр message в message-row
     template:
         '<div>' +
             '<message-row v-for="message in messages" :message="message" :key="message.id" ></message-row>' +
-        '</div>'  //:message="message" предает параметр message в message-row
+        '</div>',
+    //created - это хук который срабатывает перед рендером
+    created: function () {
+        //запрос на сервер
+        messageApi.get().then(result =>
+            result.json().then(data =>
+                //вывести данные в консоль
+                /*console.log(data),*/
+                //записать данные в messages
+                data.forEach(message => this.messages.push(message))
+            )
+        )
+    }
 })
-
 
 var app = new Vue({
     el: '#app',
     template: '<messages-list :messages="messages"/>',   //чтобы использовать компонент добавлем его в template: , :messages="messages" предает параметр message в message-list
     data: {
         messages: [
-            {id: '123', text:'Wow'},
-            {id: '124', text:'hey'},
+            {id: '123', name:'Wow'},
+            {id: '124', name:'hey'},
         ]
     }
 })
