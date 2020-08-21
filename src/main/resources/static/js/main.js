8bc3d0660fef02344ef8822673883620036b7ff8
Lastzlo <53399137+Lastzlo@users.noreply.github.com>
2020-08-21 13:35:17 +0300

    main.js added Vue component messages-list

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,6 +1,20 @@
+
+
+
+// Определяем новый компонент под именем messages-list
+Vue.component('messages-list', {    //название компонента
+    props: ['messages'],    //список значений которые мы ожидаем
+    template: '<div><div v-for="message in messages">{{ message.text }}</div></div>'
+})
+
+
 var app = new Vue({
     el: '#app',
+    template: '<messages-list :messages="messages"/>',   //чтобы использовать компонент добавлем его в template: , чтобы передать значения пишем :messages="messages"
     data: {
-        message: 'Привет, Vue!'
+        messages: [
+            {id: '123', text:'Wow'},
+            {id: '124', text:'hey'},
+        ]
     }
 })
