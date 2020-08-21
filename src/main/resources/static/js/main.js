314d4fd1d6f1909774ab10d6efeee5ec36a5933b
Lastzlo <53399137+Lastzlo@users.noreply.github.com>
2020-08-21 13:51:35 +0300

    main.js added Vue component message-row, start preparations for vue-resourse

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,16 +1,26 @@
-
+//комопонент отвечат за вивод одного сообщеня
+Vue.component(
+    'message-row',
+    {
+        props: ['message'],
+        template: '<div><i>({{ message.id }})</i> {{ message.text }}</div>',
+    }
+)
 
 
 // Определяем новый компонент под именем messages-list
 Vue.component('messages-list', {    //название компонента
     props: ['messages'],    //список значений которые мы ожидаем
-    template: '<div><div v-for="message in messages">{{ message.text }}</div></div>'
+    template:
+        '<div>' +
+            '<message-row v-for="message in messages" :message="message" :key="message.id" ></message-row>' +
+        '</div>'  //:message="message" предает параметр message в message-row
 })
 
 
 var app = new Vue({
     el: '#app',
-    template: '<messages-list :messages="messages"/>',   //чтобы использовать компонент добавлем его в template: , чтобы передать значения пишем :messages="messages"
+    template: '<messages-list :messages="messages"/>',   //чтобы использовать компонент добавлем его в template: , :messages="messages" предает параметр message в message-list
     data: {
         messages: [
             {id: '123', text:'Wow'},
