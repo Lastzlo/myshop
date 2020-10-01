25813de5cd472fc720f946e468b1cd5dde0b766e
Lastzlo <53399137+Lastzlo@users.noreply.github.com>
2020-10-01 17:43:23 +0300

    update MainPage.vue add list cards with Category

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import Vuetify from 'vuetify'
 import VueResource from 'vue-resource'
 import App from 'pages/App.vue'
-import MainPage from "pages/MainPage.vue"
+import MainPage from "pages/MainPage.vue";
 
 
 Vue.use(Vuetify)
--- src/main/resources/static/js/pages/MainPage.vue
+++ src/main/resources/static/js/pages/MainPage.vue
@@ -3,7 +3,7 @@
 
         <v-app-bar app dense>
 
-            <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+            <v-app-bar-nav-icon @click.stop="catalogDrawer = !catalogDrawer"></v-app-bar-nav-icon>
 
             <v-btn icon>
                 <v-icon>mdi-magnify</v-icon>
@@ -17,14 +17,15 @@
 
             <v-spacer></v-spacer>
 
-            <v-btn icon>
+            <v-btn icon @click.stop="autorizationDrawer = !autorizationDrawer">
                 <v-icon>mdi-dots-vertical</v-icon>
             </v-btn>
 
         </v-app-bar>
 
         <v-navigation-drawer
-                v-model="drawer"
+                id="catalogDrawer"
+                v-model="catalogDrawer"
                 absolute
                 temporary
         >
@@ -45,6 +46,7 @@
                         v-for="item in items"
                         :key="item.title"
                         link
+                        @click="openPage(item)"
                 >
                     <v-list-item-content>
                         <v-list-item-title>{{ item.name }}</v-list-item-title>
@@ -55,7 +57,76 @@
             </v-list>
         </v-navigation-drawer>
 
+        <v-navigation-drawer
+                id="autorizationDrawer"
+                v-model="autorizationDrawer"
+                absolute
+                temporary
+                right
+        >
+            <v-list-item>
+                <v-list-item-content>
+                    <v-list-item-title class="title">
+                        Авторизация
+                    </v-list-item-title>
+                </v-list-item-content>
+            </v-list-item>
+            <v-divider></v-divider>
+
+            <v-list
+                    nav
+                    dense
+            >
+                <v-list-item-group v-model="group">
+                    <v-list-item @click="openPage(null)">
+                        <v-list-item-title>Войдти через Google</v-list-item-title>
+                    </v-list-item>
+
+                    <v-list-item @click="openPage(null)">
+                        <v-list-item-title>Войдти через Facebook</v-list-item-title>
+                    </v-list-item>
+
+                    <v-list-item @click="openPage(null)">
+                        <v-list-item-title>Регистрация</v-list-item-title>
+                    </v-list-item>
+                </v-list-item-group>
+            </v-list>
+        </v-navigation-drawer>
+
         <v-main>
+            <v-card
+            ё        class="mx-auto"
+                    max-width="768"
+            >
+                <v-container fluid>
+                    <v-row
+                            dense
+                    >
+                        <v-col
+                                v-for="item in items"
+                                :key="item.id"
+                                :cols="4"
+                        >
+                            <v-card
+                                    height="120px"
+                                    width="120px"
+                                    @click="openPage(item)"
+                            >
+                                <!--<v-img
+                                        class="white&#45;&#45;text align-end"
+                                        gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+                                        height="150px"
+                                        width="150px"
+                                >
+                                    <v-card-title v-text="item.name"></v-card-title>
+                                </v-img>-->
+
+                                <v-card-subtitle v-text="item.name"></v-card-subtitle>
+                            </v-card>
+                        </v-col>
+                    </v-row>
+                </v-container>
+            </v-card>
 
         </v-main>
 
@@ -67,14 +138,21 @@
     export default {
         name: "MainPage",
         data: () => ({
-            drawer: false,
+            catalogDrawer: false,
+            autorizationDrawer: false,
             group: null,
             items: [
             ],
         }),
+        methods:{
+            openPage(item){
+                console.log("openPage")
+            },
+        },
         watch: {
             group () {
-                this.drawer = false
+                this.catalogDrawer = false
+                this.autorizationDrawer = false
             },
         },
         created: function () {
