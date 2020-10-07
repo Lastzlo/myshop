9e38836e3a8668f20e196dd23420e067298d8bab
Lastzlo <53399137+Lastzlo@users.noreply.github.com>
2020-10-07 14:54:54 +0300

    update MvcConfig.java, when page is not found, open '/' page

--- src/main/java/com/example/myshop/config/MvcConfig.java
+++ src/main/java/com/example/myshop/config/MvcConfig.java
@@ -1,7 +1,12 @@
 package com.example.myshop.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.server.ErrorPage;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -19,6 +24,16 @@ public class MvcConfig implements WebMvcConfigurer {
 
     public void addViewControllers (ViewControllerRegistry registry){
         registry.addViewController ("/setting").setViewName ("setting.html");
+    }
+
+
+
+    @Bean
+    public WebServerFactoryCustomizer<ConfigurableServletWebServerFactory> webServerCustomizer(){
+        return container -> {
+            //добавили ErrorPage, если не находит даный адрес то перекидывает на "/"
+            container.addErrorPages (new ErrorPage (HttpStatus.NOT_FOUND, "/"));
+        };
     }
 
 }
